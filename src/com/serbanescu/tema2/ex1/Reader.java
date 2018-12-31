614040ce5b4193116db5f7cbadbc417d09870d5b
Kartuni <kartuni@gmail.com>
2018-12-31 12:19:54 +0200

    god dammit GIT

---
+++ src/com/serbanescu/tema2/ex1/Reader.java
@@ -0,0 +1,21 @@
+package com.serbanescu.tema2.ex1;
+
+import java.util.Scanner;
+
+public class Reader {
+    private Scanner reader;
+
+    public Reader() {
+        reader = new Scanner(System.in);
+    }
+
+    public double readDouble() {
+        double n = reader.nextDouble();
+        return n;
+    }
+
+    public int readInt() {
+        int n = reader.nextInt();
+        return n;
+    }
+}
